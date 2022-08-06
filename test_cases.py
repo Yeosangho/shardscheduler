ddfe0dd611dd63c5709a9ff4ccc3eb9cde3eb7b7
Seungjun Lee <dltmdwns812@daum.net>
2022-08-06 18:22:21 +0900

    test adaptive sdp schedule

--- test_cases.py
+++ test_cases.py
@@ -775,7 +775,7 @@ def make_schedule_from_json(params_list, scheduled_comms_init , scheduled_comms,
 				idx = comp['idx'] if 'idx' in comp else None
 				#find comp is scheduled in previous steps
 				exist_task = None			
-				for task in task_dict['comp_type'] :
+				for task in task_dict[comp_type] :
 					if(task.idx == idx ):
 						exist_task = task
 				if(exist_task == None):
@@ -800,8 +800,6 @@ def make_schedule_from_json(params_list, scheduled_comms_init , scheduled_comms,
 	scheduled_comms.extedn(bw_ops)
 
 
-
-	#Init scheudle -> Exclude RS, AR from BWTOFW and FW
 
 
 	for comm in scheduled_comms:
