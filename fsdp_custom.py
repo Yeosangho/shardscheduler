0fe0d35d0201b1dc41429f145ad24834a2e2f53e
Seungjun Lee <dltmdwns812@daum.net>
2022-08-07 16:40:56 +0900

    test adaptive sdp schedule

--- fsdp_custom.py
+++ fsdp_custom.py
@@ -1306,6 +1306,9 @@ class FullyShardedDataParallel(nn.Module):
             return t
 
         # Attach hooks to Tensor outputs.
+        print(outputs)
+        import os 
+        os._exit(0)
         outputs = apply_to_tensors(_register_hook, outputs)
 
         return outputs
