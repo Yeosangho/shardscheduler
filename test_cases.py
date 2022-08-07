ee4d3927a75b620aa5142f2e16fe4eabcde406a3
Seungjun Lee <dltmdwns812@daum.net>
2022-08-07 15:06:47 +0900

    test adaptive sdp schedule

--- test_cases.py
+++ test_cases.py
@@ -851,12 +851,12 @@ def make_schedule_from_json(params_list, scheduled_comms_init , scheduled_comms,
 				
 
 
-	#for comm in scheduled_comms:
-	#	print(comm)
+	for comm in scheduled_comms_init:
+		print(comm)
 	#print(task_init_list)
 	#print(task_dict['BWTOFW'])
-	#import os
-	#os._exit(0)
+	import os
+	os._exit(0)
 	for task in scheduled_comms :
 		for comm in task.comms :
 			for param_wrap in comm.params : 
