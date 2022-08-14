bc826a310aaea0b60c08af63ac7a1923eb1798c3
Seungjun Lee <dltmdwns812@daum.net>
2022-08-14 16:53:07 +0900

    test adaptive sdp

--- main.py
+++ main.py
@@ -244,8 +244,8 @@ class Trainer:
 			fsdp_num = count - len_module
 
 		fsdp_num = 0
-		sdp_num =  int(len_module)  
-		dp_num = 0
+		sdp_num = 0    
+		dp_num = int(len_module)
 		adaptive_sdp = {}
 		adaptive_sdp['FSDP'] = fsdp_num
 		adaptive_sdp['DP'] = dp_num
