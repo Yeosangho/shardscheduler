b49cbe37292827161fb8a208efd669fc51877122
Seungjun Lee <dltmdwns812@daum.net>
2022-08-14 18:39:40 +0900

    test adaptive sdp

--- test_cases.py
+++ test_cases.py
@@ -755,7 +755,7 @@ def make_schedule_from_json(params_list, scheduled_comms_init , scheduled_comms,
 				target_comm_params = []
 				for comm in comp['scheduled_comm'][comm_op]:
 					param = params_list[int(comm['idx'])]
-
+#
 					start_ratio = 0.0
 					end_ratio = 0.0
 					if(comm['org_size'] == comm['param']):
@@ -769,6 +769,8 @@ def make_schedule_from_json(params_list, scheduled_comms_init , scheduled_comms,
 							start_ratio = 0.0
 						current_ratio = comm['param'] / comm['org_size']
 						end_ratio = round(start_ratio + current_ratio, 4)
+						if(abs(end_ratio - 1) <= 0.0001 and abs(end_ratio -1) > 0.0):
+							end_ratio = 1.0 
 						if( start_ratio < end_ratio):
 							target_comm_params.append(PartiableParam(param, start_ratio, end_ratio, comm['idx']))
 							comm_ratio[comm_op][param] = end_ratio
