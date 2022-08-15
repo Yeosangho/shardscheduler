d3c713d0618865408bd39a0f2d115e8ffe7ec77c
Seungjun Lee <dltmdwns812@daum.net>
2022-08-15 20:09:39 +0900

    latency penalty version

--- torch_scheduler.py
+++ torch_scheduler.py
@@ -470,9 +470,9 @@ class ShardScheduler(torch.optim.Optimizer):
                             #p.grad = None  
     
                             param = param_wrap.param  
-                            #if(param_wrap.start_idx == 0):
-                            #    print( param.grad.data[:param_wrap.shard_size].size())
-                            #    param._full_param_padded.data.storage().resize_( param.grad.data[:param_wrap.shard_size].size()[0])
+                            if(param_wrap.start_idx == 0):
+                                print( param.grad.data[:param_wrap.shard_size].size())
+                                param._full_param_padded.data.storage().resize_( param.grad.data[:param_wrap.shard_size].size()[0])
                             param._full_param_padded.data[param_wrap.start_idx:param_wrap.end_idx].copy_(self.bucket.shard_buffer[pre_offset:offset])
                             pre_offset = offset
                             count += 1
