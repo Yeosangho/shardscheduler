a7eb655b515c2167c72ce0516e8948662837cb4f
Seungjun Lee <dltmdwns812@daum.net>
2022-10-23 19:23:20 +0900

    add empty cache

--- sdp_custom.py
+++ sdp_custom.py
@@ -1202,7 +1202,7 @@ class ShardedDataParallel(nn.Module):
         # also ensures that after the first forward, the optimizer state will be
         # initialized with the correct dtype and (sharded) size, since optimizer
         # state is typically initialized lazily in ``optim.step()``.
-        self._use_fp32_param_shard()
+        #self._use_fp32_param_shard()
         torch.cuda.empty_cache()
         # Register pre-backward hooks to all-gather the params for the backward
         # pass (if output's grad was needed). This won't register anything if
