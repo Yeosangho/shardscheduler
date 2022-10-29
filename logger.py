43fc5828e07d8d3066a924b87cc70cd4d12ada55
Seungjun Lee <dltmdwns812@daum.net>
2022-10-29 19:01:57 +0900

    test trial log

--- logger.py
+++ logger.py
@@ -3,4 +3,4 @@ import csv
 def write_trial(trial_info):
 	f = open(f'exp_{trial_info["exp_tag"]}.csv','a+', newline='')
 	wr = csv.writer(f)
-	wr.writerow(trial_info)
+	wr.writerow([trial_info["bucket_size"], trial_info["dp"], trial_info["sdp"], trial_info["fsdp"], trial_info["time"]])
