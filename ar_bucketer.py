390ee2532caf1292f0edfdb2146694386d92580f
shyeo <soboru963@ajou.ac.kr>
2023-01-07 13:30:40 +0000

    test

--- ar_bucketer.py
+++ ar_bucketer.py
@@ -119,16 +119,17 @@ class ARBucketer:
         #customlogging.debug(self.rank, f"after allreduce {param_name} :: {torch.sum(param.grad.data)}")
         #customlogging.debug(self.rank, f"scheduled params is fully communicated  param {param_name}, start_idx {start_idx}, end_idx {end_idx}, org_size {org_size}")
         param.grad.data[start_idx:end_idx].copy_(self.fusion_buffer[pre_offset:offset])
-        #self.synced_param_num_dict[param] += end_idx - start_idx
-        #if self.synced_param_num_dict[param]  == param._orig_size.numel():            
-        #    self.optimizer._adam(param)
-        #    self.optimizer._zero_one_grad(param)
-        #    self.synced_param_num_dict[param]  = 0
-        if param_count + end_idx - start_idx == param._orig_size.numel():
-            self.optimizer._adam(param)
-            self.optimizer._zero_one_grad(param)               
-        param_count += end_idx - start_idx
         
+        if self.synced_param_num_dict[param] + end_idx - start_idx  == param._orig_size.numel():            
+            self.optimizer._adam(param)
+            self.optimizer._zero_one_grad(param)
+            #self.synced_param_num_dict[param]  = 0
+            if(torch.sum(param.grad) == 0):
+                self.synced_param_num_dict[param] += end_idx - start_idx
+        #if param_count + end_idx - start_idx == param._orig_size.numel():
+        #    self.optimizer._adam(param)
+        #    self.optimizer._zero_one_grad(param)               
+
     def iterative_push(self, param, grad, param_name, start_idx, end_idx, org_size, shard_size, commType, callback_fn):
         remains, start_idx = self.push( param, grad, param_name, start_idx, end_idx, org_size, shard_size, commType)
         while remains > 0:
@@ -155,8 +156,8 @@ class ARBucketer:
         self.fusion_buffer[self.offset : self.offset + param_num-remains].copy_(grad[start_idx : end_idx-remains]) 
         self.pre_offset = self.offset
         self.offset += param_num - remains
-        param_count = self.synced_param_num_dict[param]
-        p = Param(param, start_idx, end_idx-remains, org_size, shard_size, grad=grad,  param_name=param_name, pre_offset=self.pre_offset, offset=self.offset, param_count=param_count)
+        #param_count = self.synced_param_num_dict[param]
+        p = Param(param, start_idx, end_idx-remains, org_size, shard_size, grad=grad,  param_name=param_name, pre_offset=self.pre_offset, offset=self.offset)
         callback_fn = functools.partial(self.post_allreduce, p)
         self.params.add(p, callback_fn)
 
